b800845f7c360a0f61c1a45f1d3f14ec4cc0f900
Bartosz Elert <60293321+volcente@users.noreply.github.com>
2025-02-22 15:22:22 +0100

    Add sqlc (#3)
    * added sqlc, postgres DB integration to user handlers
    
    * minor fixes based on the solution files

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -3,7 +3,7 @@ CREATE TABLE users (
   id uuid PRIMARY KEY,
   created_at timestamp NOT NULL,
   updated_at timestamp NOT NULL,
-  name text NOT NULL
+  name text NOT NULL UNIQUE
 );
 
 -- +goose Down
