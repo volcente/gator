00584f3828a029d8a8e8c6729acad8669e4d8287
Bartosz Elert <60293321+volcente@users.noreply.github.com>
2025-02-22 16:09:07 +0100

    added users command (#5)

--- main.go
+++ main.go
@@ -34,6 +34,7 @@ func main() {
 	}
 	cmds.register("login", handlerLogin)
 	cmds.register("register", handlerRegister)
+	cmds.register("users", handlerUsers)
 	cmds.register("reset", handlerReset)
 
 	if len(os.Args) < 2 {
