3e5792df1bc9fedace5f3f30509e2e5efc0d88ff
Bartosz Elert <elert.bartosz@gmail.com>
2025-03-02 19:03:24 +0100

    added pagination for browse command

--- sql/queries/posts.sql
+++ sql/queries/posts.sql
@@ -4,7 +4,9 @@ INSERT INTO posts (id, created_at, updated_at, title, url, description,
 VALUES ($1, $2, $3, $4, $5, $6, $7, $8);
 
 -- name: GetPostsForUser :many
-SELECT *
-FROM posts
-ORDER BY published_at DESC
-LIMIT $1;
+SELECT p.title, p.published_at, p.description, p.url, p.feed_id
+FROM posts AS p
+INNER JOIN feeds as f ON p.feed_id = f.id
+ORDER BY p.published_at DESC
+LIMIT $1
+OFFSET $2;
